ac78f291c239ea7d285d44aeccf0ca43de037c43
Erman Louie Faminiano <koresensi.299@gmail.com>
2023-08-07 16:27:19 +0800

    Lesson 1 done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var Inputnum1 = document.querySelector("#input1");
 var Inputnum2 = document.querySelector("#input2");
 var SubmitButton = document.querySelector("button");
-function addNumbers(num1, num2) {
+function addnumbers(num1, num2) {
     return num1 + num2;
 }
 SubmitButton === null || SubmitButton === void 0 ? void 0 : SubmitButton.addEventListener("click", function () {
-    console.log('Total $(addNumbers(+Inputnum1.value, +Inputnum2.value))');
+    console.log("The sum is ".concat(addnumbers(+Inputnum1.value, +Inputnum2.value)));
 });
--- app.ts
+++ app.ts
@@ -2,11 +2,20 @@ const Inputnum1 = document.querySelector("#input1");
 const Inputnum2 = document.querySelector("#input2");
 const SubmitButton = document.querySelector("button");
 
-function addNumbers(num1 : number , num2 : number) {
+function addnumbers(num1 : number , num2 : number) {
     return num1 + num2;
 }
 
-SubmitButton === null || SubmitButton === void 0 ? void 0 : SubmitButton.addEventListener("click", ()=> {
-    console.log('Total $(addNumbers(+Inputnum1.value, +Inputnum2.value))');
-     });
+SubmitButton?.addEventListener("click", ()=> {
+    console.log(`The sum is ${addnumbers(+Inputnum1.value, +Inputnum2.value)}`);
+     }
+     );
+
+
+     
+
+
+
+
+
 

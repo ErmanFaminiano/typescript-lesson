d583f6dd5fd78eff2c9ed6a5044024fc17934f9e
Erman Louie Faminiano <koresensi.299@gmail.com>
2023-08-07 15:55:00 +0800

    added new code

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello, ErmanTS");
+var Inputnum1 = document.querySelector("#input1");
+var Inputnum2 = document.querySelector("#input2");
+var SubmitButton = document.querySelector("button");
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+SubmitButton === null || SubmitButton === void 0 ? void 0 : SubmitButton.addEventListener("click", function () {
+    console.log('Total $(addNumbers(+Inputnum1.value, +Inputnum2.value))');
+});
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("Hello, ErmanTS");
+const Inputnum1 = document.querySelector("#input1");
+const Inputnum2 = document.querySelector("#input2");
+const SubmitButton = document.querySelector("button");
+
+function addNumbers(num1 : number , num2 : number) {
+    return num1 + num2;
+}
+
+SubmitButton === null || SubmitButton === void 0 ? void 0 : SubmitButton.addEventListener("click", ()=> {
+    console.log('Total $(addNumbers(+Inputnum1.value, +Inputnum2.value))');
+     });
+

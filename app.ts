798c12a4f80ec25fe4c5586198e3a3f1496d7d07
Erman Louie Faminiano <koresensi.299@gmail.com>
2023-08-17 14:13:44 +0800

    unit testing with ts-jest

--- app.ts
+++ app.ts
@@ -1,14 +1,1 @@
-const Inputnum1 = document.querySelector("#input1");
-const Inputnum2 = document.querySelector("#input2");
-const SubmitButton = document.querySelector("#but1");
-const playButton = document.querySelector("#but2");
-var age: number = 10;
-
-function addnumbers(num1: number, num2: number) {
-  return num1 + num2;
-}
-
-SubmitButton?.addEventListener("click", () => {
-  console.log(`The sum is ${addnumbers(+Inputnum1.value, +Inputnum2.value)}`);
-  console.log(`I am ${age} years of age`);
-});
+console.log("Hello, World");
